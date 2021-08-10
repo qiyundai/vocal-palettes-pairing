ee2d30bf2a35ac8d1d73f65631ed5a06bbfd6bc3
Qiyun Dai <pianoquin@gmail.com>
2021-08-10 18:04:04 -0500

    project setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vocal Palettes & Pairing</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+</body>
+
+<footer>
+    <script type="text/javascript" src="script.js"></script>
+</footer>
+</html>
