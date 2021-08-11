d773565126c54b6e6027fc5ab22c57075e4eb1c4
Qiyun Dai <pianoquin@gmail.com>
2021-08-11 11:15:13 -0500

    Layout finished

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 <body>
     <div class="container">
         <div class="container__content">
+            <img src="./assets/poster.png" alt="">
             <h1 class="title">Petite Opera Presents Vocal Palettes & Pairings</h1>
             <p>
                 Join Petite Opera for this fabulous event that educates your palate, expands your musical palette and engages all of your senses over the course of one afternoon.
@@ -34,17 +35,26 @@
                 <label for="party">Number of Guest(s)</label>
                 <div class="container__form__party">
                     <input id="party" name="party" type="number" required>
-                    <p>Donation* per person: $100.00</p>
+                    <div>Donation* per person: $100.00</div>
                 </div>
-                <button>Submit</button>
+                <button>Submit and Donate via<img src="assets/paypal-brands.svg" alt="paypal"></button>
             </form>
             <p>- or -</p>
-            <a class="link-donate" href="">I just want to donate</a>
+            <a class="link-donate" href="https://paypal.com/us/fundraiser/charity/1263109" target="_blank">I just want to donate</a>
+        </div>
+        <div class="footnote">
+            <p>
+            * Donation covers tastings of 11 wines, a concert featuring 10 professional singers, hor d’oeuvres and accompanying wine and vocal notes. Petite Opera is a 501(c)3 charitable organization. Donations are tax deductible to the extent of the law. 
+            </p>
+            <p>
+                Donations made via credit card made to PayPal Giving Fund which accepts all credit cards No PayPal account necessary. For other means of donating, please see petiteopera.org/donate or email us at donations@petiteopera.org
+            </p>
         </div>
     </div>
 </body>
 
 <footer>
+    <p class="copyright"></>©Qiyun Dai</></p>
     <script type="text/javascript" src="script.js"></script>
 </footer>
 </html>
