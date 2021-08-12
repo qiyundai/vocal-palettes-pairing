71812cba3ad0213525024ad22de4b60f60b9a254
Qiyun Dai <pianoquin@gmail.com>
2021-08-11 22:39:17 -0500

    ENV bug fix

--- donation.php
+++ donation.php
@@ -16,8 +16,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = ENV['GMAIL_USER_NAME'];                     //SMTP username
-    $mail->Password   = ENV['GMAIL_APP_PASSWORD'];                               //SMTP password
+    $mail->Username   = $_ENV['GMAIL_USER_NAME'];                     //SMTP username
+    $mail->Password   = $_ENV['GMAIL_APP_PASSWORD'];                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
